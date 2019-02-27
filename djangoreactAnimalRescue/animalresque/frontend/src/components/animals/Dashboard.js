9cffb44c441efb52c7e1a016bdc904855e2e9951
Manuel <manuel2_m@hotmail.com>
2019-02-27 12:43:17 -0400

    Adding a class component with a clock dynamic

--- djangoreactAnimalRescue/animalresque/frontend/src/components/animals/Dashboard.js
+++ djangoreactAnimalRescue/animalresque/frontend/src/components/animals/Dashboard.js
@@ -1,12 +1,13 @@
 import React, { Fragment } from 'react';
 import Form from './Form';
 import Animal from './Animal';
+import ProfileImage from './ProfileImage';
+
 
 export default function Dashboard() {
 	return (
 		<Fragment>
 			<Form />
-			<Animal />
 		</Fragment>
 	)
 }
